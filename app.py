994a2d3c95e18a7a385773739fffb325a4899e33
10BI <binary101011001@gmail.com>
2026-09-05 13:11:58 +0500

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import os
 
 from flask import Flask, render_template, request, send_file
 from PIL import Image
@@ -40,4 +41,5 @@ def remove_background():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
